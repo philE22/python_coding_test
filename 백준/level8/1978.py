a9e2e605d69a47ae232116264c1aaf4d6e21b745
philE22 <moonwhlking@gmail.com>
2022-12-27 21:37:24 +0900

    백준 1978문제 추가

--- 백준/level8/1978.py
+++ 백준/level8/1978.py
@@ -2,5 +2,17 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
+print(arr)
+count = 0
 for i in arr:
-    
+    check = True
+    if i == 1:
+        continue
+    for j in range(2, i):
+        if i%j == 0:
+            check = False
+            break
+    if check:
+        count += 1
+
+print(count)

87bee3769826beb8d35e9b2dd039ff39e5e277ad
philE22 <moonwhlking@gmail.com>
2023-01-04 23:11:48 +0900

    2738 문풀 추가

--- 백준/level9/2738.py
+++ 백준/level9/2738.py
@@ -1,14 +1,14 @@
 n, m = map(int, input().split())
 
-arr1 = [list(map(int, input().split())) for i in range(m)]
-arr2 = [list(map(int, input().split())) for i in range(m)]
+arr1 = [list(map(int, input().split())) for i in range(n)]
+arr2 = [list(map(int, input().split())) for i in range(n)]
 
-result = [[0 for i in range(n)] for j in range(m)]
+result = [[0 for i in range(m)] for j in range(n)]
 
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         result[i][j] = str(arr1[i][j] + arr2[i][j])
         
-for i in range(m):
+for i in range(n):
     print(" ".join(result[i]))
     

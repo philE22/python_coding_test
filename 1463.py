c914edbf219cf746b6e1cd04e11b2a0ada93d725
philE22 <moonwhlking@gmail.com>
2023-02-25 00:42:15 +0900

    1463 코드리팩터링

--- 1463.py
+++ 1463.py
@@ -1,20 +1,17 @@
 N = int(input())
 
-
 dp = [0] * (N + 1)
 dp[1] = 0
-n = 3
+n = 2
+
 #dp[n] = min(dp[n / 3] + 1, dp[n / 2] + 1, dp[n - 1] + 1)
-arr = []
+
 while n <= N:
+    dp[n] = dp[n - 1] + 1
     if n % 3 == 0:
-        arr.append(dp[n // 3] + 1)
+        dp[n] = min(dp[n // 3] + 1, dp[n])
     if n % 2 == 0:
-        arr.append(dp[n // 2] + 1)
-    arr.append(dp[n - 1] + 1)
-    
-    dp[n] = min(arr)
-    arr.clear()
+        dp[n] = min(dp[n // 2] + 1, dp[n])
     n += 1
 
 print(dp[N])
